5229598edf646b97698d491b3959917c833d4940
Luiz Fernando Andrade De Souza <lfsouza3@stefanini.com>
2017-09-19 14:36:07 -0300

    Add header and footer with social media

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductsComponent } from './products/products.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RegisterComponent,
+    ContactComponent,
+    ServicesComponent,
+    ProductsComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'register', component: RegisterComponent},
+      {path:'products', component: ProductsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
